ad6d428664bfd1139c5b7633f41cfd8db2180e43
andijayawizard <andijayawizard@gmail.com>
2022-06-15 17:28:14 +0700

    1506

---
+++ database/migrations/2022_06_15_090331_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable()->default('name');
+            $table->text('description')->nullable();
+            $table->enum('pub', ['yes', 'no'])->nullable()->default('yes');
+            $table->bigInteger('category_id')->nullable()->default(12)->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
